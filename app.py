26bc5e2c02fe4c78f9a958f9d7eaf53b1fb9837e
Manthan Patel <77008504+Manthan404@users.noreply.github.com>
2022-09-11 15:59:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from crypt import methods
 from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-import pickle
 
 # load the model from disk
-filename = 'nlp_model.pkl'
-sgd_clf = pickle.load(open(filename, 'rb'))
+filename = 'mr.pkl'
+clf = pickle.load(open(filename, 'rb'))
 cv=pickle.load(open('tranform.pkl','rb'))
 app = Flask(__name__)
 
@@ -16,28 +12,15 @@ app = Flask(__name__)
 def home():
 	return render_template('home.html')
 
-
 @app.route('/predict',methods=['POST'])
 def predict():
-
-	messagess = request.form['message']
-	data = [messagess]
-	vect = cv.tranform(data).toarray()
-	my_prediction = sgd_clf.predict(vect)
-
+	if request.method == 'POST':
+		message = request.form['message']
+		data = [message]
+		vect = cv.transform(data).toarray()
+		my_prediction = clf.predict(vect)
 	return render_template('result.html',prediction = my_prediction)
-	
 
-# @app.route('/predict',methods=['POST'])
-# def predict():
-    
-#     if request.method == 'POST':
-		
-# 		message = request.form['message']
-# 		data = [message]
-# 		vect = cv.transform(data).toarray()
-# 		my_prediction = sgd_clf.predict(vect)
-# 	return render_template('result.html',prediction = my_prediction)
 
 
 if __name__ == '__main__':
