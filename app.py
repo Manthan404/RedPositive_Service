83b52e88388f864e42ef5103e17b05550ce9413c
Manthan Patel <77008504+Manthan404@users.noreply.github.com>
2022-09-11 16:50:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,82 @@
-from flask import Flask,render_template,url_for,request
-import pandas as pd 
-import pickle
+from flask import Flask, render_template, session, redirect, url_for, session
+from flask_wtf import FlaskForm
+from wtforms import TextField,SubmitField
+from wtforms.validators import NumberRange
 
-# load the model from disk
-filename = 'mr.pkl'
-clf = pickle.load(open(filename, 'rb'))
-cv=pickle.load(open('tranform.pkl','rb'))
+import numpy as np  
+from tensorflow.keras.models import load_model
+import joblib
+
+
+
+def return_prediction(model,scaler,sample_json):
+    
+    # For larger data features, you should probably write a for loop
+    # That builds out this array for you
+    
+    s_len = sample_json['sepal_length']
+    
+    flower = [[s_len]]
+    
+    flower = scaler.transform(flower)
+    
+    classes = np.array(['positive', 'negative'])
+    
+    class_ind = model.predict_classes(flower)
+    
+    return classes[class_ind][0]
+
+
+
 app = Flask(__name__)
+# Configure a secret SECRET_KEY
+# We will later learn much better ways to do this!!
+app.config['SECRET_KEY'] = 'someRandomKey'
 
-@app.route('/')
-def home():
-	return render_template('home.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
-	if request.method == 'POST':
-		message = request.form['message']
-		data = [message]
-		vect = cv.transform(data).toarray()
-		my_prediction = clf.predict(vect)
-	return render_template('result.html',prediction = my_prediction)
+# REMEMBER TO LOAD THE MODEL AND THE SCALER!
+flower_model = load_model("movie_review_model.h5")
+flower_scaler = joblib.load("movie_review.pkl")
 
+
+# Now create a WTForm Class
+# Lots of fields available:
+# http://wtforms.readthedocs.io/en/stable/fields.html
+class FlowerForm(FlaskForm):
+    sep_len = TextField('Sepal Length')
+
+    submit = SubmitField('Analyze')
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    # Create instance of the form.
+    form = FlowerForm()
+    # If the form is valid on submission (we'll talk about validation next)
+    if form.validate_on_submit():
+        # Grab the data from the breed on the form.
+
+        session['sep_len'] = form.sep_len.data
+
+        return redirect(url_for("prediction"))
+
+
+    return render_template('home.html', form=form)
+
+
+@app.route('/prediction')
+def prediction():
+
+    content = {}
+
+    content['sepal_length'] = float(session['sep_len'])
+
+    results = return_prediction(model=flower_model,scaler=flower_scaler,sample_json=content)
+
+    return render_template('prediction.html',results=results)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
